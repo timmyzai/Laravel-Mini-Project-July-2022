4951054469365e1c01b49346d93b3dd47f4646a2
Timmy1011 <timmycdc@gmail.com>
2022-07-19 00:00:21 +0800

    added controllers and views
    added bootstraps

---
+++ student-management-system/resources/views/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+<h4 class="pb-3">Students List</h4>
+
+<div class="card">
+    <div class="card-header">
+        First Student
+    </div>
+    <div class="card-body">
+        <div class="card-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, qui voluptate quibusdam harum inventore ab dolor obcaecati officia, nulla molestias quaerat maxime libero voluptas soluta saepe iusto quis, magnam voluptatem?
+        </div>
+    </div>
+</div>
+@endsection
